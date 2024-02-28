d4454d4c74107cca2bd304c1db7793de34bb5e18
Lee Jeong Won <106802869+zwei1garden@users.noreply.github.com>
2024-02-29 02:26:22 +0900

    Update send.sh

--- worker/worker2/send.sh
+++ worker/worker2/send.sh
@@ -2,6 +2,6 @@
 
 new_filename="worker2_$(date +"%Y%m%d_%H:%M:%S")"
 
-scp /home/worker2/sandbox/log/resource.log save-server@172.27.0.2:/home/save-server/save/worker2/${new_filename}.log
+scp /home/worker2/sandbox/log/resource.log save-server@172.27.0.8:/home/save-server/save/worker2/${new_filename}.log
 
 rm /home/worker2/sandbox/log/resource.log
