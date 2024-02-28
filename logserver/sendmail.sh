0542415f24cb4c6a63c68e3807c452763f9be2d2
Lee Jeong Won <106802869+zwei1garden@users.noreply.github.com>
2024-02-29 02:21:27 +0900

    Update sendmail.sh

--- logserver/sendmail.sh
+++ logserver/sendmail.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
 #receiver
-receiver="right1magic@swu.ac.kr, dlwjddnjs081723@gmail.com, ajh29464@swu.ac.kr, babi815@swu.ac.kr"
+receiver="right1magic@swu.ac.kr, dlwjddnjs081723@naver.com, ajh29464@swu.ac.kr, babi815@swu.ac.kr"
 
 #folder
 server1folder="/home/save-server/save/server1"
 server2folder="/home/save-server/save/server2"
 server3folder="/home/save-server/save/server3"
-
 
 #latest
 latest_server1=$(ls -t "$server1folder" | head -n1)
@@ -15,12 +14,14 @@ latest_server2=$(ls -t "$server2folder" | head -n1)
 latest_server3=$(ls -t "$server3folder" | head -n1)
 
 #subjects
-subject="logfile"
+subject_ser1="server1-logfile"
+subject_ser2="server2-logfile"
+subject_ser3="server3-logfile"
 
 #content
 content="this is logfile."
 
 #send
-echo "$content" | mutt -s "$subject" -a "$server1folder/$latest_server1" -- "$receiver"
-echo "$content" | mutt -s "$subject" -a "$server2folder/$latest_server2" -- "$receiver"
-echo "$content" | mutt -s "$subject" -a "$server3folder/$latest_server3" -- "$receiver"
+echo "$content" | mutt -s "$subject_ser1" -a "$server1folder/$latest_server1" -- "$receiver"
+echo "$content" | mutt -s "$subject_ser2" -a "$server2folder/$latest_server2" -- "$receiver"
+echo "$content" | mutt -s "$subject_ser3" -a "$server3folder/$latest_server3" -- "$receiver"
