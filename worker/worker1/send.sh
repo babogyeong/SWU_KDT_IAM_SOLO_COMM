74717686312c97d05a4eb5297425937e366b8d1c
babogyeong <122768710+babogyeong@users.noreply.github.com>
2024-02-29 01:40:50 +0900

    Update send.sh

--- worker/worker1/send.sh
+++ worker/worker1/send.sh
@@ -2,7 +2,7 @@
 
 new_filename="server1_$(date +"%Y%m%d_%H:%M:%S")"
 
-scp /home/server1/sandbox/log/resource.log save-server@172.27.0.8:/home/save-server/save/server1/${new_filename}.log
+scp /home/worker1/sandbox/log/resource.log save-server@172.27.0.8:/home/save-server/save/worker1/${new_filename}.log
 
-rm /home/server1/sandbox/log/resource.log
+rm /home/worker1/sandbox/log/resource.log
 
