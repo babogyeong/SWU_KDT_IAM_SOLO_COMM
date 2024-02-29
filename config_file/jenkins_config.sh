f4b0dee3ce5fbfb195ca4718b0d98050e8eba185
babogyeong <122768710+babogyeong@users.noreply.github.com>
2024-02-29 09:47:02 +0900

    Create jenkins_config.sh
    젠킨스 설정파일 추가

---
+++ config_file/jenkins_config.sh
@@ -0,0 +1,4 @@
+# apt update
+# apt install openjdk-17-jdk -y
+# curl -fsSL https://pkg.jenkins.io/debian/jenkins.io-2023.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+# apt install jenkins
